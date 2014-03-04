c7fae0aea78d7cabca3b1951f6954f624277edc4
Andy <andrew.farrell@phgroup.com>
2014-03-04 19:03:48 +0000

    Add option for 'WGL4' meta-block, and refactor query code to account for it.

--- controllers/chars.js
+++ controllers/chars.js
@@ -33,12 +33,15 @@ exports.blocks = function (req, res) {
 exports.search = function (req, res) {
   // Returns JSON array of characters based on given search criteria.
 
-  var queryParams,
+  var queryParams = [],
     select = 'SELECT code, name, alt_name, wgl4 FROM chars WHERE ',
-    where;
+    selectWithBlock = 'SELECT chars.code, chars.name, chars.alt_name, chars.wgl4, blocks.name AS block, blocks.id AS block_id FROM chars INNER JOIN blocks ON chars.block_id = blocks.id WHERE ',
+    where,
+    charName = req.query.name || '',
+    blockId = +(req.query.block_id || '');
 
   // If both name and block_id are missing, we don't know what to search for.
-  if (!req.query.name && !req.query.block_id) {
+  if (!charName && !blockId) {
     return res.send(400);
   }
 
@@ -52,20 +55,36 @@ exports.search = function (req, res) {
   // won't like rendering a list that big. TODO: pagination!
   // Also, with no block ID specified in a search, we return the block name and
   // ID so it can be linked to in the HTML result.
-  if (req.query.name && !req.query.block_id) {
-    select = 'SELECT chars.code, chars.name, chars.alt_name, chars.wgl4, blocks.name AS block, blocks.id AS block_id FROM chars INNER JOIN blocks ON chars.block_id = blocks.id WHERE ';
-    where = 'chars.name LIKE $1 OR chars.alt_name LIKE $1 ORDER BY code LIMIT 300';
+
+  if (blockId < 1) {
+    select = selectWithBlock;
+  }
+
+  if (blockId) {
+    // Real block ID.
+    if (charName) {
+      if (blockId === -1) {
+        // Char name and 'WGL4' meta-block.
+        where = '(name LIKE $1 OR alt_name LIKE $1) AND wgl4 = true ORDER BY code';
+        queryParams = ['%' + charName.toUpperCase() + '%'];
+      } else {
+        // char name and real block ID.
+        where = '(name LIKE $1 OR alt_name LIKE $1) AND block_id = $2 ORDER BY code';
+        queryParams = ['%' + charName.toUpperCase() + '%', blockId];
+      }
+    } else {
+      // Block ID but no char name.
+      if (blockId === -1) {
+        where = 'wgl4 = true ORDER BY code';
+      } else {
+        where = 'block_id = $1 ORDER BY code';
+        queryParams = [blockId];
+      }
+    }
+  } else {
+    // No block ID, implying only char name.
+    where = 'chars.name LIKE $1 OR chars.alt_name LIKE $1 ORDER BY code LIMIT 500';
     queryParams = ['%' + req.query.name.toUpperCase() + '%'];
-  }
-
-  if (req.query.name && req.query.block_id) {
-    where = '(name LIKE $1 OR alt_name LIKE $1) AND block_id = $2 ORDER BY code';
-    queryParams = ['%' + req.query.name.toUpperCase() + '%', +req.query.block_id];
-  }
-
-  if (req.query.block_id && !req.query.name) {
-    where = 'block_id = $1 ORDER BY code';
-    queryParams = [+req.query.block_id];
   }
 
   console.log(select, where);
